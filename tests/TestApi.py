0370eca17f96253f945b79dc019634ed4b15420b
AnastasiiaKravets <kravecanastasiia@gmail.com>
2026-06-24 12:48:16 +0300

    Rung only API tests, disable playwright browsers installing

--- tests/TestApi.py
+++ tests/TestApi.py
@@ -28,6 +28,7 @@ def created_pet(api_client):
 
 class TestApi:
 
+    @pytest.mark.gate_api
     def test_create_pet(self, api_client):
         payload = PetCreateModelRequest(id=1, name="Doggie")
         print(payload.model_dump_json())
@@ -42,6 +43,7 @@ class TestApi:
         assert not created_pet.photoUrls, "Photo urls should be empty"
         assert not created_pet.tags, "Tags should be empty"
 
+    @pytest.mark.gate_api
     def test_get_pet(self, created_pet: PetCreateModelRequest, api_client):
         result = api_client.get(f"pet/{created_pet.id}")
         assert result.status_code == 200
@@ -53,6 +55,7 @@ class TestApi:
         assert pet.photoUrls == created_pet.photoUrls, "Photo urls should be empty"
         assert pet.tags == created_pet.tags, "Tags should be empty"
 
+    @pytest.mark.gate_api
     def test_update_pet(self, created_pet: PetCreateModelRequest, api_client):
         updated_pet = created_pet
         updated_pet.name = created_pet.name + "_Updated"
@@ -67,6 +70,7 @@ class TestApi:
         assert pet.photoUrls == updated_pet.photoUrls, "Photo urls should be empty"
         assert pet.tags == updated_pet.tags, "Tags should be empty"
 
+    @pytest.mark.gate_api
     def test_delete_pet(self, created_pet: PetCreateModelRequest, api_client):
         result = api_client.delete(f"pet/{created_pet.id}")
         assert result.status_code == 200
@@ -76,6 +80,7 @@ class TestApi:
         PetErrorResponse.model_validate(get_result.json())
 
 
+    @pytest.mark.gate_api
     @pytest.mark.parametrize("status", [pet_status.value for pet_status in PetStatus])
     def test_get_pet_by_status(self, api_client, status: PetStatus):
         result = api_client.get("/pet/findByStatus", params={"status": status})
